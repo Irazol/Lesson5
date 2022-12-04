5a5381d5157a39540ffd7633e24fe903bac92cd9
Irazol <118620892+Irazol@users.noreply.github.com>
2022-12-04 20:37:06 +0200

    Add files via upload

---
+++ Array5.java
@@ -0,0 +1,26 @@
+package Lesson5;
+
+//вывести последовательность фибоначи. до того числа которое введет пользователь(индекс)
+import java.util.Scanner;
+public class Array5 {
+    public static void main(String[] args) {
+
+        int[] array = new int[1000];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введіть якесь число n:");
+        int n = input.nextInt();
+
+        for(int i = 0; i <array.length; i++){
+            if(i==0)array[i] =0;
+            else if(i==1)array[i] =1;
+            else if(i>1) array[i] = array[i-2]+array[i-1];
+        }
+
+        System.out.print("Послідовність Фібоначчі по індекс " + n +" включно  = ");
+        int i = 0;
+        while(i<n){
+            System.out.print(array[i] + " ");
+            i++;
+        }
+    }
+}
